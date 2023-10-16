d7f1b5b47ead092434c65f9d8eb2516545185540
Arthur Lorenzzo de Barros Oliveira <arthur.oliveira@solutis.net.br>
2023-10-16 14:08:46 -0300

    Criacao do microsservico

---
+++ src/main/java/com/br/livepaypag/LivePayPagApplication.java
@@ -0,0 +1,13 @@
+package com.br.livepaypag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivePayPagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivePayPagApplication.class, args);
+    }
+
+}
